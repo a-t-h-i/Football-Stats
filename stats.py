8928e38fa9c4b18b12894d457ecdc963f970edeb
a-t-h-i <aonlysmith@gmail.com>
2023-02-15 17:47:41 +0000

    Added code to save teams from csv to list of teams

--- stats.py
+++ stats.py
@@ -1,4 +1,51 @@
-#This is the main package
 from scraper import scraper
+import csv
+import os
+import pandas as pd
+os.system('clear')
 
-scraper.getHTML("https://www.premierleague.com/tables")
+class stats(object):
+    def __init__(self, home, away):
+        self.df = pd.read_csv('csv/soccer-standings.csv', header=1)
+        self.homeTeam = home
+        self.awayTeam = away
+        self.rows, self.columns = self.df.shape
+        self.teams = []
+
+
+    def mapTeams(self):
+        for i in range(self.rows-1):
+            self.teams.append(self.df.loc[i, :].values.flatten().tolist()) #Append to list of teams
+        
+
+    def teamFound(self,searchValue):
+        for team in self.teams:
+
+            if team[0].upper() == searchValue.upper():
+                return True
+
+        return False
+
+
+def teams():
+    home = input("Home team: ")
+    away = input("Away team: ")
+    return (home, away)
+
+homeTeam, awayTeam = teams()
+run = stats(homeTeam, awayTeam) #Instantiate stats class
+run.mapTeams() #Save list of teams from CSV file
+found = False
+
+while not found:
+
+    if run.teamFound(homeTeam) and run.teamFound(awayTeam):
+        found = True
+    else:
+        os.system('clear')
+        print("Please enter correct team names...")
+        homeTeam, awayTeam = teams()
+
+
+# scraper.getHTML("https://www.rotowire.com/soccer/league-table.php")
+
