c7b969dae049c5706374ca336fb097df84c56e0d
a-t-h-i <aonlysmith@gmail.com>
2023-02-16 10:17:42 +0000

    Coded getters for team object

--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ import pandas as pd
 os.system('clear')
 
 class team(object):
-    def __init__(object, name, position, data):
+    def __init__(self, name, position, data):
         self.name = name
         self.position = position
         self.data = data
@@ -17,24 +17,22 @@ class team(object):
         return self.name
 
     def getGamesPlayed(self):
-        #Code to get games played from data
-        return 0
+        return self.data[3]
     
     def getGamesWon(self):
-        #Code to get games won
-        return 0
+        return self.data[4]
+    
+    def getLoses(self):
+        return self.data[6]
 
     def getDraws(self):
-        #Code to get number of times drawn
-        return 0
+        return self.data[5]
 
     def getGoals(self):
-        #Code to get number of goals scored
-        return 0
+        return self.data[7]
 
     def getGoalsConceded(self):
-        #Code to get goals conceded
-        return 0
+        return self.data[8]
 
 
 class stats(object):
@@ -63,6 +61,7 @@ class stats(object):
         return self.teams
 
 
+
 def teams():
     home = input("Home team: ")
     away = input("Away team: ")
@@ -71,11 +70,25 @@ def teams():
 homeTeam, awayTeam = teams()
 run = stats(homeTeam, awayTeam) #Instantiate stats class
 run.mapTeams() #Save list of teams from CSV file
+teams = run.getTeams()
+teamObjects = []
+
+#Add teams to list of objects
+for i in range(len(teams)-1):
+    teamObjects.append(team(teams[i][0], i+1, teams[i]))
+
 found = False
 
 while not found:
 
     if run.teamFound(homeTeam) and run.teamFound(awayTeam):
+        print(f"Name: {teamObjects[0].getName()}")
+        print(f"Posision: {teamObjects[0].getPosition()}")
+        print(f"Matches won: {teamObjects[0].getGamesWon()}")
+        print(f"Draws: {teamObjects[0].getDraws()}")
+        print(f"Conceded: {teamObjects[0].getGoalsConceded()}")
+        print(f"Loses: {teamObjects[0].getLoses()}")
+        print(f"Played: {teamObjects[0].getGamesPlayed()}")
         found = True
     else:
         os.system('clear')
