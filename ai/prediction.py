fcc379c50cb73477454b23da9e288fa5b073cb71
a-t-h-i <aonlysmith@gmail.com>
2023-02-20 19:57:22 +0000

    Changed temperature for more realistic and accurate predictions

--- ai/prediction.py
+++ ai/prediction.py
@@ -1,17 +1,17 @@
 import openai
 import os
 #You can get your open ai api key here: https://platform.openai.com/account/api-keys
-openai.api_key = ""
+openai.api_key = "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"
 
 def ask(query):
     
     completions = openai.Completion.create(
     engine="text-davinci-003",
     prompt=query,
-    max_tokens=1024,
+    max_tokens=512,
     n=1,
     stop=None,
-    temperature=0.5,)
+    temperature=0.4,)
 
     response = completions.choices[0].text
     return response    
