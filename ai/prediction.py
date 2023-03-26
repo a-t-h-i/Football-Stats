c8d927f959b4213657e4dbb2df66ffddbd66cf53
a-t-h-i <aonlysmith@gmail.comc>
2023-03-26 14:49:40 +0200

    Yah nne, build failed because of a stupid typo

--- ai/prediction.py
+++ ai/prediction.py
@@ -17,19 +17,4 @@ def ask(query):
     temperature=0.6,)
 
     response = completions.choices[0].text
-    return response    
-
-
-if not os.path.exisYts("token.json"):
-    print("Whoops! Looks like you have not yet set up tour AI key.")
-    print("You can get your open ai api key here: https://platform.openai.com/account/api-keys")
-    key = input("Please enter your open ai key here: ")
-    while not is_valid_key(key):
-        print("The open ai key you entered is not valid.")
-        key = input("Please enter your open ai key here: ")
-    with open('token.json','w+') as file:
-        json.dump({"ai_key":key},file)
-
-with open('token.json','r') as file:
-    data = json.load(file)
-    openai.api_key = data["ai_key"]
+    return response
