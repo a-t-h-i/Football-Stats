a44309b893e341d82e57d21e08f772800ae55414
Athenkosi Hlubi <110525368+a-t-h-i@users.noreply.github.com>
2023-08-13 17:57:14 +0200

    Update calculate.py
    Updated calculation logic to avoid dividing by zero...

--- calc/calculate.py
+++ calc/calculate.py
@@ -1,14 +1,27 @@
 def average_goals(goals_scored, games_played):
+    if (goals_scored = 0) or (games_played = 0):
+        return 0.0;
+        
     return float(round((int(goals_scored) / int(games_played)), 2))
 
 
 def win_percentage(games_played, games_won):
+    if (games_played = 0) or (games_won = 0):
+        return 0.0;
+        
     return float(round(((int(games_won) / int(games_played)) * 100), 2))
 
 
 def lose_percentage(games_played, games_lost):
+    if (games_played = 0) or (games_lost = 0):
+        return 0.0;
+        
     return float(round(((int(games_lost) / int(games_played)) * 100), 2))
+    
 
 
 def draw_percentage(games_played, draws):
+    if (games_played = 0) or (draws = 0):
+        return 0.0;
+    
     return float(round(((int(draws) / int(games_played)) * 100), 2))
