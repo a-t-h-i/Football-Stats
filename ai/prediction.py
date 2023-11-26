67433048778bfb97d332051a88cf6eb3197959e4
Athenkosi Hlubi <110525368+a-t-h-i@users.noreply.github.com>
2023-11-26 16:26:31 +0200

    Update prediction.py
    Updated ai model

--- ai/prediction.py
+++ ai/prediction.py
@@ -7,7 +7,7 @@ openai.api_key = os.environ.get("MY_KEY")
 
 def ask(query):
     completions = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-3.5-turbo",
         prompt=query,
         max_tokens=512,
         n=1,
